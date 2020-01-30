b8c76ed7d3f181a410fff5005e8e14c039d3813c
dhill30 <56419223+dhill30@users.noreply.github.com>
2020-01-30 02:02:17 -0800

    Delete health.js

--- health.js
+++
@@ -1,9 +0,0 @@
-function health(gameEntity, entityHealth) {
-    console.log("health test");
-    this.theEntity = gameEntity;
-    this.currentHealth = entityHealth;
-}
-
-health.prototype.takeDamage = function(damageAmount){
-    this.currentHealth -= damageAmount;
-}
